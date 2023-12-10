52e30a01e17aa2573bcef54df2c4059a6ff3e9e4
harini18122005 <2005hariniramesh@gmail.com>
2023-12-10 23:44:43 +0530

    done

---
+++ earth_boilerplate-main/app.js
@@ -0,0 +1,9 @@
+const $climate= document.getElementById("climate");
+const $consumption=document.getElementById("consumption");
+const $resources= document.getElementById("resources");
+const $people= Document.getElementById("people");
+
+$climate.oneclick=() =>{
+document.body.style.background=
+   "url('https://github.com/Kalvium-Program/earth')";
+}
